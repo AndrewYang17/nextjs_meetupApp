58e37b1fc26983e7a1f493cfb9c6cced58bb8181
AndrewYang17 <yangzhisiangg@gmail.com>
2021-04-27 21:39:41 +0800

    commit

---
+++ src/pages/Favourites.js
@@ -0,0 +1,23 @@
+import { useContext } from "react";
+import FavouritesContext from "../store/favourites-context";
+import MeetupList from "../components/meetups/MeetupList";
+
+const FavouritesPage = () => {
+  const favouriteCtx = useContext(FavouritesContext);
+
+  let content;
+
+  if (favouriteCtx.totalFavourites === 0) {
+    content = <p>You got no favourites yet. Start adding some?</p>;
+  } else {
+    content = <MeetupList meetups={favouriteCtx.favourites} />;
+  }
+  return (
+    <section>
+      <h1>My Favourites</h1>
+      {content}
+    </section>
+  );
+};
+
+export default FavouritesPage;
